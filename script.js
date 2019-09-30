9ab399ebc3206c521cbf2a1a8cc722ec544632d4
jess2896 <jesscastillo28@hotmail.com>
2019-09-30 11:15:26 -0400

    Fixed the code's format

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-let johnTeamScores=[89,20,103];
-let mikeTeamScores=[116,94,123];
+let johnTeamScores = [89,20,103];
+let mikeTeamScores = [116,94,123];
+let johnsAvrg = calcAverage(johnTeamScores);
+let mikesAvrg = calcAverage(mikeTeamScores);
+let hasHigherScore = johnsAvrg > mikesAvrg? "John's team has a higher average of " + johnsAvrg : "Mike's team has a higher average of " + mikesAvrg;
+let avrg = johnsAvrg == mikesAvrg? "John's and Mike's teams have the same average score" : hasHigherScore;
 
-function calcAverage(scores){
-    let sum=0;
-    for(let i=0;i<scores.length;i++){
-        sum+=scores[i];
-    }
+function calcAverage(scores) {
+    let sum = 0;
+    scores.forEach(score => {
+        sum += score;
+    });
     return sum/scores.length;
 }
 
-let johnsAvrg = calcAverage(johnTeamScores);
-let mikesAvrg = calcAverage(mikeTeamScores);
-let hasHigherScore = johnsAvrg>mikesAvrg? "John's team has a higher average of "+johnsAvrg: "Mike's team has a higher average of "+mikesAvrg;
-let avrg = johnsAvrg == mikesAvrg? "John's and Mike's teams have the same average score": hasHigherScore;
-
 //Extra
 
-let maryTeamScores=[97,134,105];
+let maryTeamScores = [97,134,105];
 let maryAvrg = calcAverage(maryTeamScores);
-let winner="The three teams are tied";
+let winner = "The three teams are tied";
 
-if(maryAvrg>johnsAvrg && maryAvrg>mikesAvrg){
-    winner= "Mary's team has the highest average score of "+maryAvrg;
+if (maryAvrg > johnsAvrg && maryAvrg> mikesAvrg) {
+    winner = "Mary's team has the highest average score of " + maryAvrg;
 }
-else if(maryAvrg!==johnsAvrg && maryAvrg!==mikesAvrg){
-    winner=avrg;
+else if (maryAvrg !== johnsAvrg && maryAvrg !== mikesAvrg) {
+    winner = avrg;
 }
-else if(maryAvrg>johnsAvrg && maryAvrg==mikesAvrg){
+else if (maryAvrg > johnsAvrg && maryAvrg == mikesAvrg) {
     winner = "Mary's and Mike's teams have the highest score";
 }
-else if(maryAvrg>mikesAvrg && maryAvrg==johnsAvrg){
+else if (maryAvrg > mikesAvrg && maryAvrg == johnsAvrg) {
     winner = "Mary's and John's teams have the highest score";
 }
 
