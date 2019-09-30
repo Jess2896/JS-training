221614922ff87b620a623e38c07199ee1a913a9e
jess2896 <jesscastillo28@hotmail.com>
2019-09-30 11:32:12 -0400

    Fixed the code's format

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-let bills=[124,48,268];
+const bills = [124, 48, 268];
+const allTips = [];
+const finalAmounts = [];
 
-function tipCalc(bill){
-    if(bill<50){
-        return bill*0.2;
+function tipCalc(bill) {
+
+    if (bill < 50) {
+        return bill * 0.2;
     }
-    else if(bill<200){
-        return bill*0.15;
+    
+    else if (bill < 200) {
+        return bill * 0.15;
     }
-    else{
-        return bill*0.1;
+
+    else {
+        return bill * 0.1;
     }
 }
 
-let allTips=[];
-let finalAmounts=[];
+bills.forEach(bill => {
 
-for(let i=0;i<bills.length;i++){
-    allTips.push(tipCalc(bills[i]));
-    finalAmounts.push(allTips[i]+bills[i]);
-}
+    const tip = tipCalc(bill);
+    allTips.push(tip);
+    finalAmounts.push(tip + bill);
+});
+
+//testing
 
 console.log(allTips);
 console.log(finalAmounts);
